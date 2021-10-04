871d3353c23f10cb3a68a32d02110a8367c81458
patrick <zipat@t-online.de>
2021-10-04 11:20:48 +0200

    Merge execution logic into main

--- mediacopier-cli/src/main.cpp
+++ mediacopier-cli/src/main.cpp
@@ -1,4 +1,4 @@
-/* Copyright (C) 2020-2021 Patrick Ziegler
+/* Copyright (C) 2021 Patrick Ziegler
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -14,17 +14,127 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-#include "cli/SequentialExecutor.hpp"
+#include "ConfigManager.hpp"
+
+#include <mediacopier/AbstractFileInfo.hpp>
+#include <mediacopier/Error.hpp>
+#include <mediacopier/FileOperationMove.hpp>
+#include <mediacopier/FileOperationMoveJpeg.hpp>
+#include <mediacopier/FileRegister.hpp>
 
 #include <spdlog/spdlog.h>
 
+#include <atomic>
+#include <csignal>
+#include <filesystem>
+#include <functional>
+
 namespace fs = std::filesystem;
+
+static volatile std::atomic<bool> operationCancelled;
+
+static void abortable_wrapper(std::function<void()> abortable_function)
+{
+    operationCancelled.store(false);
+    std::signal(SIGINT, [](int signal) -> void {
+        (void) signal;
+        operationCancelled.store(true);
+    });
+    abortable_function();
+    std::signal(SIGINT, SIG_DFL);
+}
+
+template <typename T>
+static void execute(const MediaCopier::FileRegister& fileRegister)
+{
+    for (const auto& [destination, file] : fileRegister) {
+        try {
+            T operation{destination};
+            file->accept(operation);
+
+        }  catch (const MediaCopier::FileOperationError& err) {
+            spdlog::warn(std::string{err.what()} + ": " + file->path().string());
+        }
+
+        if (operationCancelled.load()) {
+            spdlog::info("Aborting execution");
+            break;
+        }
+    }
+}
+
+namespace MediaCopier::Cli {
+
+static auto run(const ConfigManager& config) -> void
+{
+    if (!fs::is_directory(config.inputDir)) {
+        throw MediaCopierError("Input folder does not exist");
+    }
+
+    FileRegister fileRegister{config.outputDir, config.pattern};
+
+    for (const auto& file : fs::recursive_directory_iterator(config.inputDir)) {
+        if (file.is_regular_file()) {
+            try {
+                fileRegister.add(file);
+            } catch (const FileInfoError& err) {
+                spdlog::warn(std::string{err.what()} + ": " + file.path().string());
+            }
+        }
+    }
+
+    if (fileRegister.size() < 1) {
+        throw MediaCopierError("No files were found in " + config.inputDir.string());
+    }
+
+    spdlog::info("Found " + std::to_string(fileRegister.size()) + " files");
+
+    switch (config.command)
+    {
+    case ConfigManager::Command::COPY:
+        spdlog::info("Executing COPY operation");
+        abortable_wrapper([fileRegister]() -> void {
+            execute<FileOperationCopy>(fileRegister);
+        });
+        break;
+
+    case ConfigManager::Command::MOVE:
+        spdlog::info("Executing MOVE operation");
+        abortable_wrapper([fileRegister]() -> void {
+            execute<FileOperationMove>(fileRegister);
+        });
+        break;
+
+    case ConfigManager::Command::COPY_JPEG:
+        spdlog::info("Executing COPY operation (with JPEG awareness)");
+        abortable_wrapper([fileRegister]() -> void {
+            execute<FileOperationCopyJpeg>(fileRegister);
+        });
+        break;
+
+    case ConfigManager::Command::MOVE_JPEG:
+        spdlog::info("Executing MOVE operation (with JPEG awareness)");
+        abortable_wrapper([fileRegister]() -> void {
+            execute<FileOperationMoveJpeg>(fileRegister);
+        });
+        break;
+
+    default:
+        throw MediaCopierError("Unknown operation type");
+    }
+
+    spdlog::info("Execution finished");
+}
+
+} // namespace MediaCopier::Cli
 
 int main(int argc, char *argv[])
 {
+    using namespace MediaCopier::Cli;
+
     try {
-        MediaCopier::Cli::SequentialExecutor executor{{argc, argv}};
-        executor.run();
+        ConfigManager config{argc, argv};
+        run(config);
     } catch (const std::exception& err) {
         spdlog::error(err.what());
         return 1;
