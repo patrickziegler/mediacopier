e86e5fcb027b29480cab73ad8c64db9231ab3b50
patrick <zipat@t-online.de>
2022-03-12 15:22:13 +0100

    Add basic graphical user interface

--- mediacopier-cli/lang/lang_de.ts
+++ mediacopier-cli/lang/lang_de.ts
@@ -2,51 +2,96 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="de_DE">
 <context>
+    <name>MediaCopierDialog</name>
+    <message>
+        <location filename="../src/gui/dialog.ui" line="14"/>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/dialog.ui" line="35"/>
+        <source>Folder</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/dialog.ui" line="47"/>
+        <source>Input</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/dialog.ui" line="70"/>
+        <source>Output</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/dialog.ui" line="113"/>
+        <source>Parameter</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/dialog.ui" line="125"/>
+        <source>Operation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/dialog.ui" line="135"/>
+        <source>Base Format</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/dialog.ui" line="166"/>
+        <source>Log</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>MediaCopierJob</name>
     <message>
         <location filename="../src/kde/mediacopierjob.cpp" line="44"/>
         <source>Source</source>
-        <translation>Quelle</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/kde/mediacopierjob.cpp" line="46"/>
         <source>Destination</source>
-        <translation>Ziel</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>QObject</name>
     <message>
-        <location filename="../src/core/config.cpp" line="76"/>
+        <location filename="../src/core/config.cpp" line="80"/>
+        <location filename="../src/gui/dialog.cpp" line="110"/>
         <source>Source folder</source>
-        <translation>Quellverzeichnis</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/core/config.cpp" line="79"/>
+        <location filename="../src/core/config.cpp" line="82"/>
+        <location filename="../src/gui/dialog.cpp" line="116"/>
         <source>Destination folder</source>
-        <translation>Zielverzeichnis</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/core/config.cpp" line="162"/>
-        <location filename="../src/core/config.cpp" line="165"/>
+        <location filename="../src/core/config.cpp" line="170"/>
+        <location filename="../src/core/config.cpp" line="173"/>
         <source>Copy</source>
-        <translation>Kopieren</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/core/config.cpp" line="168"/>
-        <location filename="../src/core/config.cpp" line="171"/>
+        <location filename="../src/core/config.cpp" line="176"/>
+        <location filename="../src/core/config.cpp" line="179"/>
         <source>Move</source>
-        <translation>Verschieben</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/core/config.cpp" line="174"/>
+        <location filename="../src/core/config.cpp" line="182"/>
         <source>Show</source>
-        <translation>Zeigen</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/core/config.cpp" line="177"/>
+        <location filename="../src/core/config.cpp" line="185"/>
         <source>Unknown</source>
-        <translation>Unbekannt</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
