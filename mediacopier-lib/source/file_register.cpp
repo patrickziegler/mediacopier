1a5f3b25623162712c955030c63aaf42d91e16b7
Patrick Ziegler <zipat@proton.me>
2022-11-26 00:24:12 +0100

    Use data section when comparing jpegs

--- mediacopier-lib/source/file_register.cpp
+++ mediacopier-lib/source/file_register.cpp
@@ -28,30 +28,67 @@ namespace fs = std::filesystem;
 constexpr static const size_t BUFFER_SIZE = 1024;
 constexpr static const size_t CHUNKS_MAX = 32;
 
+static auto seek_jpeg_data(std::ifstream* input) noexcept -> bool
+{
+    uint8_t s1 = 0, s2 = 0;
+    uint16_t buf = 0;
+
+    input->seekg(2, std::ios_base::beg);
+
+    while (input->good()) {
+        input->read(reinterpret_cast<char*>(&buf), 2);
+        if (buf == 0xdaff) {
+            return true;
+        }
+        input->read(reinterpret_cast<char*>(&s1), 1);
+        input->read(reinterpret_cast<char*>(&s2), 1);
+        input->seekg(256 * s1 + s2 - 2, std::ios_base::cur);
+    }
+
+    return false;
+}
+
+static auto prepare_input_stream(const fs::path& file) -> std::unique_ptr<std::ifstream>
+{
+    if (!fs::exists(file)) {
+        throw std::runtime_error(file.string() + " does not exist");
+    }
+    auto input = std::make_unique<std::ifstream>(file,  std::ios_base::in | std::ios_base::binary);
+
+    uint16_t buf;
+    input->read(reinterpret_cast<char*>(&buf), 2);
+
+    if (buf == 0xd8ff) { // is jpeg file
+        seek_jpeg_data(input.get());
+    }
+    return input;
+}
+
 static auto is_duplicate(const fs::path& file1, const fs::path& file2) -> bool
 {
-    std::vector<char> buffer(BUFFER_SIZE, '\0');
+    const auto input1 = prepare_input_stream(file1);
+    const auto input2 = prepare_input_stream(file2);
 
-    std::ifstream input1(file1,  std::ios::in | std::ios::binary);
-    std::ifstream input2(file2,  std::ios::in | std::ios::binary);
+    std::vector<char> buffer(BUFFER_SIZE, '\0');
 
     std::string chunk1, chunk2;
     size_t chunks_left = CHUNKS_MAX;
 
-    while (!(input1.fail() || input2.fail()) && chunks_left > 0) {
-        input1.read(buffer.data(), BUFFER_SIZE);
-        chunk1 = {buffer.begin(), buffer.begin() + input1.gcount()};
+    while (chunks_left > 0) {
+        if (!input1->good() && !input2->good()) {
+            return false;
+        }
+        input1->read(buffer.data(), BUFFER_SIZE);
+        chunk1 = {buffer.begin(), buffer.begin() + input1->gcount()};
 
-        input2.read(buffer.data(), BUFFER_SIZE);
-        chunk2 = {buffer.begin(), buffer.begin() + input2.gcount()};
+        input2->read(buffer.data(), BUFFER_SIZE);
+        chunk2 = {buffer.begin(), buffer.begin() + input2->gcount()};
 
         if (chunk1 != chunk2) {
             return false;
         }
-
         --chunks_left;
     }
-
     return true;
 }
 
