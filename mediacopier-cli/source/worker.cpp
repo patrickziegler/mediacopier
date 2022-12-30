ed3bcc694fb589e1caf2c0947e7354bdcbbf070b
Patrick Ziegler <zipat@proton.me>
2022-12-30 15:52:27 +0100

    Merge core header into worker

--- mediacopier-cli/source/worker.cpp
+++ mediacopier-cli/source/worker.cpp
@@ -14,12 +14,24 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-#include "core.hpp"
 #include "worker.hpp"
 
 #ifdef ENABLE_KDE
 #include <kde/KMediaCopierJob.hpp>
 #endif
+
+#include <mediacopier/file_info_factory.hpp>
+#include <mediacopier/file_register.hpp>
+
+#include <mediacopier/operation_copy.hpp>
+#include <mediacopier/operation_copy_jpeg.hpp>
+#include <mediacopier/operation_move.hpp>
+#include <mediacopier/operation_move_jpeg.hpp>
+#include <mediacopier/operation_simulate.hpp>
+
+#include <range/v3/view/filter.hpp>
+#include <range/v3/view/transform.hpp>
+#include <range/v3/iterator_range.hpp>
 
 #include <spdlog/spdlog.h>
 #include <spdlog/sinks/basic_file_sink.h>
@@ -33,14 +45,7 @@ namespace fs = std::filesystem;
 
 namespace {
 
-using ExecFuncType = std::function<std::unique_ptr<mc::FileRegister>(const fs::path&, const fs::path&, const std::string&, std::function<void(const fs::path&, const fs::path&)>)>;
-
-static const std::map<Config::Command, ExecFuncType> execFuncMap = {
-    {Config::Command::COPY, &execute<mc::FileOperationCopy>},
-    {Config::Command::COPY_JPEG, &execute<mc::FileOperationCopyJpeg>},
-    {Config::Command::MOVE, &execute<mc::FileOperationMove>},
-    {Config::Command::MOVE_JPEG, &execute<mc::FileOperationMoveJpeg>},
-    {Config::Command::SIMULATE, &execute<mc::FileOperationSimulate>}};
+namespace mc = mediacopier;
 
 static volatile std::atomic<bool> operationCancelled(false);
 static volatile std::atomic<bool> operationSuspended(false);
@@ -62,6 +67,70 @@ auto check_operation_state()
     }
     return false;
 }
+
+auto valid_media_files(const fs::path& path)
+{
+    using namespace ranges;
+
+    static auto is_regular_file = [](const fs::directory_entry& path) {
+        return fs::is_regular_file(path);
+    };
+
+    static auto is_valid = [](const mc::FileInfoPtr& file) {
+        return file != nullptr;
+    };
+
+    return make_iterator_range(
+                fs::recursive_directory_iterator(path),
+                fs::recursive_directory_iterator())
+            | views::filter(is_regular_file)
+            | views::transform(mc::to_file_info_ptr)
+            | views::filter(is_valid);
+}
+
+auto valid_media_file_count(const fs::path& inputDir) -> size_t
+{
+    return ranges::distance(valid_media_files(inputDir));
+}
+
+template <typename Operation>
+auto execute(
+        const fs::path& inputDir,
+        const fs::path& outputDir,
+        const std::string& pattern,
+        const std::function<void(const fs::path&, const fs::path&)> callbackStatus)
+{
+    auto destRegister = std::make_unique<mc::FileRegister>(outputDir, pattern);
+    fs::path lastPath = "";
+
+    try {
+        for (auto file : valid_media_files(inputDir)) {
+            const auto destPath = destRegister->add(file);
+
+            if (destPath.has_value()) {
+                lastPath = destPath.value();
+                Operation op(destPath.value());
+                file->accept(op);
+            }
+
+            callbackStatus(file->path(), lastPath);
+        }
+
+    } catch (const std::exception& err) {
+        spdlog::error(err.what());
+    }
+
+    return std::move(destRegister);
+}
+
+using ExecFuncType = std::function<std::unique_ptr<mc::FileRegister>(const fs::path&, const fs::path&, const std::string&, std::function<void(const fs::path&, const fs::path&)>)>;
+
+static const std::map<Config::Command, ExecFuncType> execFuncMap = {
+    {Config::Command::COPY, &execute<mc::FileOperationCopy>},
+    {Config::Command::COPY_JPEG, &execute<mc::FileOperationCopyJpeg>},
+    {Config::Command::MOVE, &execute<mc::FileOperationMove>},
+    {Config::Command::MOVE_JPEG, &execute<mc::FileOperationMoveJpeg>},
+    {Config::Command::SIMULATE, &execute<mc::FileOperationSimulate>}};
 
 } // namespace
 
